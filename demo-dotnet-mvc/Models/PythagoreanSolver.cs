c8a1cb41b617419ed859089e4fc97d7ceef30869
mdazmainabrar <abrarmain.aa@gmail.com>
2026-04-15 18:58:18 +0600

    ïnitial build

---
+++ demo-dotnet-mvc/Models/PythagoreanSolver.cs
@@ -0,0 +1,13 @@
+﻿namespace demo_dotnet_mvc.Models
+{
+    public class PythagoreanSolver
+    {
+        public int SideA { get; set; }
+        public int SideB { get; set; }
+
+        public double CalculateHypotenuse()
+        {
+            return Math.Sqrt(SideA * SideA + SideB * SideB);
+        }
+    }
+}
